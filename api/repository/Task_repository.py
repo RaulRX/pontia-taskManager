494865badeab9c40ff94cf8a86ebde20d70cc093
Raul <rulox.github@gmail.com>
2026-03-16 01:03:19 +0100

    Repository and model

---
+++ api/repository/Task_repository.py
@@ -0,0 +1,75 @@
+import logging
+
+from sqlalchemy.exc import MultipleResultsFound
+
+from api.repository.config.Configuration import Initializer
+from .model.Note import Note
+from .exception.Exceptions import NoteAlreadyExistsException, NoteNotFoundException, DuplicatedNoteException
+from sqlalchemy import exists, select
+
+class Base_repository:
+
+    def __init__(self, db_config: Initializer):
+        self.__db_config = db_config
+
+class Repository(Base_repository):
+
+    logger = logging.getLogger("task_repository")
+
+    def __init__(self, configuration: Initializer):
+        super().__init__(configuration)
+
+    
+    def save_note(self, note: Note) -> None:
+        db = self.__db_config.get_session()
+        exists_note = self.__exists_by_criteria(Note.title == note.title, Note.completed == False, Note.deadline_date == None)
+
+        if exists_note:
+            raise NoteAlreadyExistsException(note.title)
+        
+        db.add(note)
+        db.commit()
+        db.refresh(note)
+        db.close()
+
+    def get_by_id(self, id: int) -> Note:
+        db = self.__db_config.get_session()
+        exists_note = self.__exists_by_criteria(Note.id == id)
+
+        if not exists_note:
+            raise NoteNotFoundException(id)
+        
+        try:
+            row = db.query(Note).filter_by(id = id).one()
+            db.close()
+
+            return row
+
+        except MultipleResultsFound:
+            raise DuplicatedNoteException(id)
+
+    def set_completed(self, id: int) -> bool:
+        db = self.__db_config.get_session()
+
+        note = self.get_by_id(id)
+        note.completed = True
+
+        db.add(note)
+        db.commit()
+        db.refresh(note)
+        db.close()
+
+        return True
+        
+    def get_expired_notes(self) -> list:
+        #TODO
+        pass
+
+    def modify(self, note: Note) -> None:
+        # TODO
+        pass
+
+    def __exists_by_criteria(self, *criteria) -> bool | None:
+        db = self.__db_config.get_session()
+        return db.scalar(select(exists().where(*criteria)))
+
---
+++ api/repository/exception/Exceptions.py
@@ -0,0 +1,14 @@
+class NoteAlreadyExistsException(Exception):
+    
+    def __init__(self, title: str):
+         super().__init__(f"Active note with title {title} already exists")
+
+class NoteNotFoundException(Exception):
+    
+    def __init__(self, id: int):
+         super().__init__(f"Note with id {id} not found")
+
+class DuplicatedNoteException(Exception):
+    
+    def __init__(self, id: int):
+         super().__init__(f"Note with id {id} has multiple results")
